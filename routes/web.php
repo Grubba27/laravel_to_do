bea21ce23dd42e6c0329717ff392d24f0f87806c
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-08-13 17:20:54 -0300

    created Tarefas Route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/tarefa', [App\Http\Controllers\TarefaController::class, 'index'])->name('tarefa');
